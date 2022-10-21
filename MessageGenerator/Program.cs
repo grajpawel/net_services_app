a73b8f7981b4ab440a94f7fa79eab19e553aa3b4
Pawel Grajkowski <pgrajkowski@automotivemastermind.com>
2022-10-21 23:26:29 +0200

    Sample request generator app

---
+++ MessageGenerator/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+namespace MessageGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddQuartz(q =>
+                    {
+                        IConfiguration configuration = hostContext.Configuration;
+
+                        q.UseMicrosoftDependencyInjectionScopedJobFactory();
+
+                        // Create a "key" for the job
+                        var jobKey = new JobKey("HelloWorldJob");
+
+                        // Register the job with the DI container
+                        q.AddJob<HelloWorldJob>(opts => opts.WithIdentity(jobKey));
+
+                        // Create a trigger for the job
+                        q.AddTrigger(opts => opts
+                            .ForJob(jobKey) // link to the HelloWorldJob
+                            .WithIdentity("HelloWorldJob-trigger") // give the trigger a unique name
+                            .WithCronSchedule("0/5 * * * * ?")); // run every 5 seconds
+
+                    });
+                    services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+                });
+        }
+    }
+}
