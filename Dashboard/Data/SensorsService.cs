e3eea8c65f8e42b228a6225e031d75026fdf3f5e
Pawel Grajkowski <pgrajkowski@automotivemastermind.com>
2022-12-05 21:58:27 +0100

    fix: fix links

--- Dashboard/Data/SensorsService.cs
+++ Dashboard/Data/SensorsService.cs
@@ -1,62 +1,54 @@
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text.Json;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Dashboard.Dtos;
-//using MessageGenerator.MessageBodies;
-
 namespace Dashboard.Data;
 
 public class SensorsService
 {
     private HttpClient _client;
+    private readonly AppOptions _appOptions;
 
-    public SensorsService(HttpClient client)
+    public SensorsService(HttpClient client, AppOptions appOptions)
     {
         _client = client;
+        _appOptions = appOptions;
     }
 
     public async Task<List<HumidityDto>> GetHumidityData()
     {
-        using HttpResponseMessage response = await _client.GetAsync("http://host.docker.internal:5001/api/Humidity");
+        using var response = await _client.GetAsync(_appOptions.HumidityApiLink);
 
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-
-        return JsonSerializer.Deserialize<List<HumidityDto>>(json);
+        return await response.Content.ReadFromJsonAsync<List<HumidityDto>>();
     }
 
     public async Task<List<PressureDto>> GetPressureData()
     {
-        using HttpResponseMessage response = await _client.GetAsync("http://host.docker.internal:5001/api/Pressure");
+        using var response = await _client.GetAsync(_appOptions.PressureApiLink);
 
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-
-        return JsonSerializer.Deserialize<List<PressureDto>>(json);
+        return await response.Content.ReadFromJsonAsync<List<PressureDto>>();
     }
 
     public async Task<List<TemperatureDto>> GetTemperatureData()
     {
-        using HttpResponseMessage response = await _client.GetAsync("http://host.docker.internal:5001/api/Temperature");
+        using var response = await _client.GetAsync(_appOptions.TemperatureApiLink);
 
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-
-        return JsonSerializer.Deserialize<List<TemperatureDto>>(json);
+        return await response.Content.ReadFromJsonAsync<List<TemperatureDto>>();
     }
 
     public async Task<List<WindDto>> GetWindData()
     {
-        using HttpResponseMessage response = await _client.GetAsync("http://host.docker.internal:5001/api/Wind");
+        using var response = await _client.GetAsync(_appOptions.WindApiLink);
 
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-
-        return JsonSerializer.Deserialize<List<WindDto>>(json);
+        return await response.Content.ReadFromJsonAsync<List<WindDto>>();
     }
 }
--- Dashboard/Startup.cs
+++ Dashboard/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,12 +21,15 @@ namespace Dashboard
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var appOptions = Configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<HttpClient>();
             services.AddSingleton<SensorsService>();
             services.AddScoped<TableController>();
+            services.AddSingleton(appOptions);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
